5f239b7378f634c19a499d6cb3c435d62cd4c564
Gao Liang <hl6349@gmail.com>
2016-05-26 21:43:40 +0800

    learning day1

---
+++ pyp/learning_day1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-       代码中中文需要加前面一段说明编码格式
+# print有括号在3.0版本里
+print  ("中文")
+# %占位符
+print("hello,%s"%'world')
+# list有序可更改 tuple有序不可改
+birth=input("birth:")
+print("birth=",birth)
+# dict字典 set无须只有值
